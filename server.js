da3c6f58a0688d0845a71544b9ef675938f13fe7
pradeep <118010705+Pradeep24032004@users.noreply.github.com>
2024-05-18 22:59:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ app.use(express.json());
 app.use(cors()); 
 
 // MongoDB connection
-mongoose.connect(DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(DATABASE_URL).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
