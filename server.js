e597fcdfeee35b74cba4e09f7523536f8877f992
pradeep <118010705+Pradeep24032004@users.noreply.github.com>
2024-05-18 23:35:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors()); 
-
+mongoose.set('strictQuery', false);
 // MongoDB connection
 mongoose.connect(DATABASE_URL, {
   useNewUrlParser: true,
